1e73ee86c953be257d5784689f6fc48a938e78ce
unknown <wil.lu@AUSYDWS88.piscesgroup.internal>
2012-03-13 15:19:37 +1100

    try

--- lift.basic/src/main/scala/bootstrap/liftweb/Boot.scala
+++ lift.basic/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -7,63 +7,66 @@ import _root_.net.liftweb.http.provider._
 import _root_.net.liftweb.sitemap._
 import _root_.net.liftweb.sitemap.Loc._
 import Helpers._
-import _root_.net.liftweb.mapper.{ DB, ConnectionManager, Schemifier, DefaultConnectionIdentifier, StandardDBVendor }
-import _root_.java.sql.{ Connection, DriverManager }
-import _root_.com.hn.lift.basic.model._
+import _root_.net.liftweb.mapper.{DB, ConnectionManager, Schemifier, DefaultConnectionIdentifier, StandardDBVendor}
+import _root_.java.sql.{Connection, DriverManager}
+import _root_.hn.model._
+
 
 /**
  * A class that's instantiated early and run.  It allows the application
  * to modify lift's environment
  */
 class Boot {
-	def boot {
-		if (!DB.jndiJdbcConnAvailable_?) {
-			val vendor =
-				new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
-					Props.get("db.url") openOr
-						"jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
-					Props.get("db.user"), Props.get("db.password"))
+  def boot {
+    if (!DB.jndiJdbcConnAvailable_?) {
+      val vendor = 
+	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
+			     Props.get("db.url") openOr 
+			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+			     Props.get("db.user"), Props.get("db.password"))
 
-			LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
+      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
 
-			DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
-		}
+      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
+    }
 
-		// where to search snippet
-		LiftRules.addToPackages("com.hn.lift.basic")
-		Schemifier.schemify(true, Schemifier.infoF _, User)
+    // where to search snippet
+    LiftRules.addToPackages("hn")
+    Schemifier.schemify(true, Schemifier.infoF _, User)
 
-		// Build SiteMap
-		def sitemap() = SiteMap(
-			Menu("Home") / "index" >> User.AddUserMenusAfter, // Simple menu form
-			// Menu with special Link
-			Menu(Loc("Static", Link(List("static"), true, "/static/index"), "Static Content")))
+    // Build SiteMap
+    def sitemap() = SiteMap(
+      Menu("Home") / "index" >> User.AddUserMenusAfter, // Simple menu form
+      // Menu with special Link
+      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
+	       "Static Content")))
 
-		LiftRules.setSiteMapFunc(() => User.sitemapMutator(sitemap()))
+    LiftRules.setSiteMapFunc(() => User.sitemapMutator(sitemap()))
 
-	/*
+
+    /*
      * Show the spinny image when an Ajax call starts
      */
-		LiftRules.ajaxStart =
-			Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+    LiftRules.ajaxStart =
+      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
 
-	/*
+    /*
      * Make the spinny image go away when it ends
      */
-		LiftRules.ajaxEnd =
-			Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+    LiftRules.ajaxEnd =
+      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
 
-		LiftRules.early.append(makeUtf8)
+    LiftRules.early.append(makeUtf8)
 
-		LiftRules.loggedInTest = Full(() => User.loggedIn_?)
+    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
 
-		S.addAround(DB.buildLoanWrapper)
-	}
+    S.addAround(DB.buildLoanWrapper)
+  }
 
-	/**
-	 * Force the request to be UTF-8
-	 */
-	private def makeUtf8(req: HTTPRequest) {
-		req.setCharacterEncoding("UTF-8")
-	}
+  /**
+   * Force the request to be UTF-8
+   */
+  private def makeUtf8(req: HTTPRequest) {
+    req.setCharacterEncoding("UTF-8")
+  }
 }
